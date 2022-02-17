bbab01728b82e5c94b2efb7929694f42523e3643
Henrique Amaral <henrique.amaral@live.com>
2022-02-17 20:25:57 -0300

    Atualiza classa CasaRet

--- CasaRet.java
+++ CasaRet.java
@@ -1,13 +1,31 @@
-class CasaRet {		
+class CasaRet {	
 /*Programa para calcular a area de uma casa (e seu cômodos)
-de 3 comodos: sala (10x10), banheiro e quarto (5x7)	
-*/							
-	public static void main (String[] args) {		
-		System.out.println("Programa para calculo da area da casa");
-		System.out.println("A AREA DA SALA E" + 10*10);
-		System.out.println("AREA DO QUARTO E" + 5*7);
-		System.out.println("A AREA DO BANHEIRO E" + 5*7);
-		System.out.println("Area da casa total" + (10*10 + 5*7 + 5*7));
+de 3 comodos: int lateral com 11 de lateral, alterando de int para float
+*/									
+	public static void main (String[] args) {	
+		
+		float lateral = 11; //alteração de 10 para 11
+		
+		float cquarto = 7; //comprimento da lateral maior do quarto
+	
+		float areaq; //area do quarto e uma variavel
+
+		float areas; //area da sala e uma variavel
+
+		float areat; //area total e uma variavel
+
+
+		System.out.println("Programama para calculo da area da casa");
+
+		areas = lateral*lateral;
+		System.out.println("Area da sala e:" + areas);
+
+		areaq = cquarto*(lateral/2);
+		System.out.println("Area do quarto e:" + areaq);
+		System.out.println("Area do quarto e:" + areaq);
+
+		areat= areas +2*areaq;
+		System.out.println("Area do quarto e:" + areat);
+
 	}
 }
-//o simbolo de mais é adicionado para CONCATENAR informacoes
