e3a7c1117967015e731348df593036e166ca608b
Henrique Amaral <henrique.amaral@live.com>
2022-02-17 16:31:51 -0300

    Implementa a classe CasaRet

---
+++ CasaRet.java
@@ -0,0 +1,13 @@
+class CasaRet {		
+/*Programa para calcular a area de uma casa (e seu cômodos)
+de 3 comodos: sala (10x10), banheiro e quarto (5x7)	
+*/							
+	public static void main (String[] args) {		
+		System.out.println("Programa para calculo da area da casa");
+		System.out.println("A AREA DA SALA E" + 10*10);
+		System.out.println("AREA DO QUARTO E" + 5*7);
+		System.out.println("A AREA DO BANHEIRO E" + 5*7);
+		System.out.println("A AREA TOTAL E" + (10*10 + 5*7 + 5*7));
+	}
+}
+//o simbolo de mais é adicionado para CONCATENAR informacoes
