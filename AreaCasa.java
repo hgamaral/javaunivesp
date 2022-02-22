fac764e5464c1ea1d8195b0fbc8e05aba3438c35
Henrique Amaral <henrique_amaral@live.com>
2022-02-22 09:02:06 -0300

    Utiliza condicionais no metodo valor para AreaCasa

--- AreaCasa.java
+++ AreaCasa.java
@@ -2,61 +2,72 @@
 
 Programa para calcular a area de uma casa (e seu cômodos) juntamente com a piscina
 
-Nessa parte do nosso programa iremos inserir ATRIBUTOS para saber o valor R$ da construção
-Algo que em softwares gerais, estaria em algum menu "Opções, "Setup" etc
-Para facilitar esse programa declaramos fora de qualquer método no programa, deixamos váriavel para
-que se permita mudanças
+Nessa parte do nosso programa iremos testar os parâmetros
 
-*/	
-								
-class AreaCasa{
-	
-		static double valorM2 = 1500; //valor do metro quadrado
-		
+SE o parâmetro for positivo ENTÃO calcule a área;
+SENÃO retorne um valor indicado erro (exemplo retorna -1)
+USANDO IF E ELSE
 
-		static void areaCasa(float lateral, float cquarto){	//método com dois argumentos, float lateral, cquarto	
+PONTOS DE ATENÇÃO: não há código após o RETURN. Nesse caso há devido
+ao condicional, porque não há como dizer antemão se haverá o retorno.
 
-		float areaq; //variavel tipo float sem valor atribuido
-		float areas; //variavel tipo float sem valor atribuido
-		float areat; //variavel tipo float sem valor atribuido
+*/
+class AreaCasa {
 
-		System.out.println("PROGRAMA PARA CASA DA AREA DA CASA RETANGULAR");
+    static double valorM2 = 1500; //valor do metro quadrado
 
-		areas = lateral*lateral;
-		System.out.println("Area da sala e:" + areas);
 
-		areaq = cquarto*(lateral/2);
-		System.out.println("Area do quarto e:" + areaq);
-		System.out.println("Area do quarto e:" + areaq);
+    static void areaCasa(float lateral, float cquarto) { //método com dois argumentos, float 			lateral, cquarto	
 
-		areat= areas +2*areaq;
-		System.out.println("Area total:" + areat);
-		}
-		
+        float areaq;
+        float areas;
+        float areat; //variavel tipo float sem valor atribuido
 
-		static double areaPiscina(double raio){ //criaco do metodo, com retorno tipo double raio		
-		return(Math.PI * Math.pow(raio,2));
-		
-		}
-		
-		static double valor(double area){
-		return(valorM2*area);
-		}
-	
-		public static void main (String[] args) {	
+        System.out.println("PROGRAMA PARA CASA DA AREA DA CASA RETANGULAR");
 
-		double areap;
-		areaCasa(11,7);
-		
-		
-		valorMetro = valor(198);
-		
-		areap = areaPiscina(2);
+        areas = lateral * lateral;
+        System.out.println("Area da sala e:" + areas);
 
-		System.out.println("Area da piscina e: " + areap);
-		
-		
+        areaq = cquarto * (lateral / 2);
+        System.out.println("Area do quarto e:" + areaq);
+        System.out.println("Area do quarto e:" + areaq);
 
-	}		
-}
-	
+        areat = areas + 2 * areaq;
+        System.out.println("Area total:" + areat);
+    }
+
+
+    static double areaPiscina(double raio) { //criaco do metodo, com retorno tipo double raio		
+        return (Math.PI * Math.pow(raio, 2));
+
+    }
+
+    static double valor(double area) {
+        if (area >= 0) 
+            return (valorM2 * area);
+            return (-1);
+
+        }
+
+        public static void main(String[] args) {
+
+            double areap;
+
+            areaCasa(11, 7);
+
+            double preco;
+
+            areap = areaPiscina(2);
+
+            System.out.println("Area da piscina e: " + areap);
+
+            preco = valor(-20);
+            if (preco >= 0)
+                System.out.println("O valor da construção e " + preco);
+            else
+                System.out.println("Valor de area negativo: ");
+
+
+
+        }
+    }
