992bdf94db1d4d01544184682b509648e2210b2f
Henrique Amaral <henrique_amaral@live.com>
2022-03-10 13:57:13 -0300

    Realiza copia de maneira incorreta de um arranjo

---
+++ Arranjos.java
@@ -0,0 +1,35 @@
+/*
+
+
+Como se utilizar cópias de arranjo e declarações corretas.
+a2 = a1; maneira incorreta de realizar uma cópia de arranjos
+
+*/
+
+class Arranjos {
+ static double valorM2 = 1500; //valor do metro quadrado
+    static double precos[] = {1500,1100,750,500}; //arranjo de double contém 5 posições 
+
+		
+
+    
+    
+
+    public static void main(String[] args) {
+	
+	int[] a1 = {0,1,2,3};
+	int[] a2 = new int[4];
+
+	a2 = a1;
+	a1[3] = 9;
+	
+	for (int val: a1)
+		System.out.println(val+", ");
+		System.out.println();
+	for (int val: a2)
+		System.out.println(val+", ");
+		System.out.println();
+
+    
+}
+}
