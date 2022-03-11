8e367f1e8e57ea6df1043710bbcfcc2cbeb596fa
Henrique Amaral <henrique_amaral@live.com>
2022-03-11 15:32:04 -0300

    Insere a maneira correta de se copiar arranjos

--- Arranjos.java
+++ Arranjos.java
@@ -1,37 +1,34 @@
 /*
-
-
 Como se utilizar cópias de arranjo e declarações corretas.
 a2 = a1; maneira incorreta de realizar uma cópia de arranjos
-
-A maneira certa de se fazer copias de arranjos é copiar termo a termo os valores do arranjo
-correspondente a a1 para o referenciado por a2
-
 */
 
 class Arranjos {
- 
+ static double valorM2 = 1500; //valor do metro quadrado
+    static double precos[] = {1500,1100,750,500}; //arranjo de double contém 5 posições 
 
-		
+
+
     public static void main(String[] args) {
-	
+
 	int[] a1 = {0,1,2,3};
 	int[] a2 = new int[4];
 
-	//a2 = a1; maneira incorreta
-	//a1[3] = 9; maneira incorreta
+	a2 = a1; //copiou o endereço da memória -- posições do array
+	//a1[3] = 9;
 
-	//copia se termo a termo... copie na primeira posição de a2 o primeiro elemento de a1 e assim por diante
+	for (int val: a1){
+		System.out.println(val+", ");
+		System.out.println();
+	}
 	
-	for (int i=0; i<a1.length; i++) a2[i] = a1[i];
-	a1[3] = 9;
-	for (int val: a1)
+	for (int val: a2){
 		System.out.println(val+", ");
 		System.out.println();
-	for (int val: a2)
-		System.out.println(val+", ");
-		System.out.println();
+	}	
+		
+		System.out.println(a1);
+		System.out.println(a2);
 
-    
 }
 }
